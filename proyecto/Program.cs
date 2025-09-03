37980a2d162e38c91aa9ac77bfdc987f28050b9f
jose luis <joseluisfernandezespinoza8@gmail.com>
2025-09-03 10:31:07 -0500

    segundo

--- proyecto/Program.cs
+++ proyecto/Program.cs
@@ -10,6 +10,17 @@ namespace proyecto
     {
         static void Main(string[] args)
         {
+            Console.Write("hola ¿cual es tu nombre?: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("¿cual es tu edad?: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("¿cual es tu estatura?: ");
+            decimal estatura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"mi nombre es {nombre}, mi edad es {edad} y mi estatura es {estatura:N2} ");
+
         }
     }
 }
